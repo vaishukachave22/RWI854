9466e37615931092042a9b85d824244028a7dadd
vaishukachave22 <127022849+vaishukachave22@users.noreply.github.com>
2024-05-24 17:14:18 +0530

    Add files via upload

--- Demo.java
+++ Demo.java
@@ -1,16 +1,14 @@
+package program;
+
 public class Demo {
-    static {
-        System.out.println("Hello World");
-    }
+	static {
+		System.out.println("Hello World");
+	}
 
-   public static void main(String[] args) {
-       System.out.println("In Main");
-       
-    
-
-   }
-   static {
-       System.out.println("Java Programming");
-   }
+	public static void main(String[] args) {
+		System.out.println("In Main");
+		// TODO Auto-generated method stub
+		
+	}
 
 }
