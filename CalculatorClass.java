26255e4b1d8f61a30b5f0524fb9196d0401f8872
vaishukachave22 <127022849+vaishukachave22@users.noreply.github.com>
2024-05-27 15:56:38 +0530

    Add files via upload

---
+++ CalculatorClass.java
@@ -0,0 +1,23 @@
+package program;
+
+class Calculator {
+	int a;
+	 public int add(int n1,int n2)
+	 {
+		 int r = n1 + n2;
+		 return r;
+	 }
+
+}
+public class CalculatorClass{
+	public static void main(String args[])
+	{
+		int num1=4;
+		int num2=5;
+		
+		Calculator cal= new Calculator();
+		int result = cal.add(num1, num2);
+		System.out.println(result);
+		
+	}
+}
