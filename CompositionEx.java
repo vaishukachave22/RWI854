302cce5eb63567d7b82b363b330e3a5a0d43f319
vaishukachave22 <127022849+vaishukachave22@users.noreply.github.com>
2024-06-05 15:29:02 +0530

    Add files via upload

---
+++ CompositionEx.java
@@ -0,0 +1,43 @@
+package RWI.Program;
+
+class CarEngine {
+    public void startEngine(){
+        System.out.println("Car Engine Started.");
+    }
+    public void stopEngine(){
+        System.out.println("Car Engine Stopped.");
+    }
+}
+ 
+class Car {
+    private String color;
+    private int max_Speed; 
+    public void carDetails(){
+        System.out.println("Car Color= "+color + "; Max Speed= " + max_Speed);
+    }
+    //set car color
+    public void setColor(String color) {
+        this.color = color;
+    }
+    //set car max_Speed
+    public void setMaxSpeed(int max_Speed) {
+        this.max_Speed = max_Speed;
+    }
+}
+ 
+class Honda extends Car{
+    public void HondaStart(){
+        CarEngine Honda_Engine = new CarEngine();   //composition
+        Honda_Engine.startEngine();
+        }
+}
+ 
+public class CompositionEx {
+    public static void main(String[] args) {        
+        Honda HondaCity = new Honda();
+        HondaCity.setColor("Silver");
+        HondaCity.setMaxSpeed(180);
+        HondaCity.carDetails();
+        HondaCity.HondaStart();
+    }
+}
